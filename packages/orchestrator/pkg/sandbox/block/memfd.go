e8ac790e9a33b51cf3ac2528d0624f134aa6ec75
Babis Chalios <babis.chalios@e2b.dev>
2026-04-22 16:38:50 +0200

    feat(memfd): parse memfd from Firecracker when passed over
    We are making a larger change to enable memfd-backed guest memory in
    Firecracker. When enabled, Firecracker passes over the memfd file
    descriptor over the UFFD UDS, alongside the UFFD file descriptor.
    
    Parse this and create a Memfd object, which we can later use to interact
    with the sandbox memory.
    
    Signed-off-by: Babis Chalios <babis.chalios@e2b.dev>

---
+++ packages/orchestrator/pkg/sandbox/block/memfd.go
@@ -0,0 +1,21 @@
+//go:build linux
+
+package block
+
+import (
+	"syscall"
+)
+
+type Memfd struct {
+	memfd int
+}
+
+func NewFromFd(fd int) *Memfd {
+	return &Memfd{
+		memfd: fd,
+	}
+}
+
+func (m *Memfd) Close() error {
+	return syscall.Close(m.memfd)
+}

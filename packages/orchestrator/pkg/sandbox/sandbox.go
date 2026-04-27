1d5f08a6bb935400ea0291a3db383c6537357dba
Babis Chalios <babis.chalios@e2b.dev>
2026-04-27 21:38:57 +0200

    feat(memfd): use memfd to export sandbox memory
    Currently, orchestrator calls process_vm_readv() system call to copy
    memory from Firecracker process into the cache backing file.
    
    Add logic to use read directly from memfd, when that is present.
    
    Signed-off-by: Babis Chalios <babis.chalios@e2b.dev>

--- packages/orchestrator/pkg/sandbox/sandbox.go
+++ packages/orchestrator/pkg/sandbox/sandbox.go
@@ -1101,6 +1101,7 @@ func (s *Sandbox) Pause(
 		memfileDiffMetadata,
 		s.config.DefaultCacheDir,
 		s.process,
+		s.memory.Memfd(ctx),
 	)
 	if err != nil {
 		return nil, fmt.Errorf("error while post processing: %w", err)
@@ -1161,13 +1162,19 @@ func pauseProcessMemory(
 	diffMetadata *header.DiffMetadata,
 	cacheDir string,
 	fc *fc.Process,
+	memfd *block.Memfd,
 ) (d build.Diff, h *header.Header, e error) {
 	ctx, span := tracer.Start(ctx, "process-memory")
 	defer span.End()
 
 	header, err := diffMetadata.ToDiffHeader(ctx, originalHeader, buildID)
 	if err != nil {
-		return nil, nil, fmt.Errorf("failed to create memfile header: %w", err)
+		var memfdErr error
+		if memfd != nil {
+			memfdErr = memfd.Close()
+		}
+
+		return nil, nil, errors.Join(fmt.Errorf("failed to create memfile header: %w", err), memfdErr)
 	}
 
 	memfileDiffPath := build.GenerateDiffCachePath(cacheDir, buildID.String(), build.Memfile)
@@ -1177,6 +1184,7 @@ func pauseProcessMemory(
 		diffMetadata.Dirty,
 		memfileDiffPath,
 		diffMetadata.BlockSize,
+		memfd,
 	)
 	if err != nil {
 		return nil, nil, fmt.Errorf("failed to export memory: %w", err)
